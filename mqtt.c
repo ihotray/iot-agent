b239a0a1a4692d5e4ed99d292eac9a69fd50bde7
shegaoyuan@aliyun.com <shegaoyuan@aliyun.com>
2025-02-14 09:34:37 +0800

    use random client id connect to mqtt server

--- mqtt.c
+++ mqtt.c
@@ -388,8 +388,6 @@ void timer_cloud_mqtt_fn(void *arg) {
 
         if (priv->cfg.opts->cloud_mqtt_client_id) {
             opts.client_id = mg_str(priv->cfg.opts->cloud_mqtt_client_id);
-        } else {
-            opts.client_id = mg_str(priv->agent_id);
         }
 
         opts.clean = true;
